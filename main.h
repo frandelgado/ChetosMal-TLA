0b8dc92891c31b45ade2a4565f96a87e42fd7f1b
lucas <lucas.emery@hotmail.com>
2018-06-29 03:40:11 -0300

    Fix main header

--- main.h
+++ main.h
@@ -1,15 +1,16 @@
 #define KEY_MAX_LENGTH (256)
 
 static const char* header = "\
+#include <stdlib.h>\n\
 #include <string.h>\n\
-#include <hashmap.h>\n\
+#include \"hashmap.h\"\n\
 #define KEY_MAX_LENGTH (256)\n\
 typedef struct {\n\
     char key[KEY_MAX_LENGTH];\n\
     double numValue;\n\
     char *strValue;\n\
 } var_t;\n\
-hashmap var_map;\n\
+map_t var_map;\n\
 void __dank_define(char * varname) {\n\
     var_t* var = malloc(sizeof(var_t));\n\
     strcpy(var->key, varname);\n\
@@ -21,7 +22,7 @@ var_t * __dank_getvar(char * varname) {\n\
     return var;\n\
 }\n\
 int main(){\n\
-    varmap = hashmap_new();\n";
+    var_map = hashmap_new();\n";
 
 static const char* footer = "\n\
     hashmap_free(var_map);\n\
