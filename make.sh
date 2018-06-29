c91c9c7a7540bd98ee0c72a50f04ad8961f52468
lucas <lucas.emery@hotmail.com>
2018-06-29 01:27:58 -0300

    Debug

--- make.sh
+++ make.sh
@@ -2,7 +2,7 @@
 echo -e "\e[33mBuilding lexer\e[0m"
 lex lexer.l
 echo -e "\e[33mBuilding parser\e[0m"
-yacc -d parser.y
+yacc --debug --verbose -d parser.y 
 echo -e "\e[33mCompiling lexer and parser\e[0m"
 cc -c lex.yy.c y.tab.c
 echo -e "\e[33mLinking lexer and parser\e[0m"
